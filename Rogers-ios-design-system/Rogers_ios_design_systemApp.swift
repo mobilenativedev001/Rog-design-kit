334f66556df0956e2f9834f57ff3262879e80d99
Yoges <yogi2study@gmail.com>
2026-05-20 14:27:19 -0400

    Initial Commit

---
+++ Rogers-ios-design-system/Rogers_ios_design_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rogers_ios_design_systemApp.swift
+//  Rogers-ios-design-system
+//
+//  Created by Yogeswaran Sivagurun - EXT on 2026-05-20.
+//
+
+import SwiftUI
+
+@main
+struct Rogers_ios_design_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
